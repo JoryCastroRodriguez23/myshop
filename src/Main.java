1d394627209cd7144346db0b7e5c2a202561ad71
Jory <cjory0921>
2024-04-06 01:20:02 +0200

    Primera version

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+package com.sanvalero.myshop;
+
+import ...
+
+
+@SpringBootApplication
+public class MyshopApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(MyshopApplication.class,args);
+
+    }
+}
